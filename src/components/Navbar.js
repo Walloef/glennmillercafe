cf6091ce6d57935aad000c03ee09f979138ba4c9
ninjagrisen <kristofer@spoton.se>
2019-01-19 18:48:41 +0100

    Images added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
@@ -46,33 +45,20 @@ const Navbar = class extends React.Component {
           </div>
           <div id="navMenu" className="navbar-menu">
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-        </Link>
-              <Link className="navbar-item" to="/meny">
+              <Link
+                activeStyle={{ fontWeight: 'bold' }}
+                className="navbar-item" to="/meny">
                 Meny
         </Link>
-              <Link className="navbar-item" to="/products">
-                Products
+              <Link
+                activeStyle={{ fontWeight: 'bold' }}
+                className="navbar-item" to="/about">
+                Om Glenn Miller Café
         </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-        </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-        </Link>
+
+
             </div>
             <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
             </div>
           </div>
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Layout from '../components/Layout'
 import { Fragment } from 'react';
 import { GoogleApiWrapper, Marker } from 'google-maps-react';
+import heroImage from './../../static/img/gmc-hero.jpg';
+import jazzSax from './../../static/img/JazzSax.jpg';
+import sax from './../../static/img/sax.jpg';
 
 import CurrentLocation from '../components/Maps';
 class IndexPage extends React.Component {
@@ -26,44 +29,18 @@ class IndexPage extends React.Component {
                   <div className="column is-10 is-offset-1">
                     <div className="content">
                       <div className="full-width-image-container margin-top-0" style={{
-                        backgroundImage: 'url(https://via.placeholder.com/450)',
+                        backgroundImage: `url(${heroImage})`,
                         backgroundSize: 'cover',
                         backgroundPosition: 'center center'
                       }}>
-                        <h2 className="has-text-weight-bold is-size-1" >Glenn Miller Café</h2></div>
+                        <h2 className="has-text-weight-bold is-size-1"
+                          style={{
+                            color: 'white',
+                            textShadow: '2px 2px 5px black'
+                          }}
+                        >Glenn Miller Café</h2></div>
                     </div>
-                    <div className="columns">
-                      <div className="column is-7">
-                        <h3 className="has-text-weight-semibold is-size-2">Hejsan</h3>
-                        <p>lorem sadlasdöasdlök asöldk saöldk aöllorem sadlasdöasdlök asöldk saöldk aöl
-                        lorem sadlasdöasdlök asöldk saöldk aöl
-                        lorem sadlasdöasdlök asöldk saöldk aöl
-                        lorem sadlasdöasdlök asöldk saöldk aöl
-                        lorem sadlasdöasdlök asöldk saöldk aöl
-                        lorem sadlasdöasdlök asöldk saöldk aöl
-                        lorem sadlasdöasdlök asöldk saöldk aöl
-                        lorem sadlasdöasdlök asöldk saöldk aöl
-                        lorem sadlasdöasdlök asöldk saöldk aöl
-                        lorem sadlasdöasdlök asöldk saöldk aöl
-                        lorem sadlasdöasdlök asöldk saöldk aöl
-                        lorem sadlasdöasdlök asöldk saöldk aöl
-                        lorem sadlasdöasdlök asöldk saöldk aöl
-                        lorem sadlasdöasdlök asöldk saöldk aöl
-                      </p>
-                      </div>
-
-                      <div className="column is-7"
-                        style={{
-                          backgroundImage: 'url(https://via.placeholder.com/450)',
-                          backgroundSize: 'cover',
-                          backgroundPosition: 'center'
-                        }}
-                      >
-
-                      </div>
-
-                    </div>
-                    <div className="columns" style={{ padding: '60px 0 30px 0' }}>
+                    <div className="columns" style={{ padding: '0 0 30px 0' }}>
                       <div className="message"
                         style={{
                           width: '500px',
@@ -78,9 +55,7 @@ class IndexPage extends React.Component {
                           <h3 className="has-text-weight-semibold is-size-2">
                             Info i korthet
                         </h3>
-                          <b>
-                            Fredagar
-                          </b>
+                          <b>Fredagar</b>
                           <p>16.30-18.00</p>
                           <b>Onsdag-söndag</b>
                           <p>18.00-01.00</p>
@@ -89,6 +64,26 @@ class IndexPage extends React.Component {
                         </div>
                       </div>
                     </div>
+                    <div className="columns">
+                      <div className="column is-7">
+                        <p>
+                          Glenn Miller Café är en världskänd lite jazzclub och restaurant i hjärtat av Stockholm. Vi har livejazzkonserter alla kvällar vi har öppet, onsdag-söndag. Restauranten öppnar kl 18 och konserterna börjar kl 20/ kl 20.30 på fredag och lördag. Menyn är franskinspirerad med musslor som vår specialitet.
+  Här spelar de stora musikerna på den lilla scenen - och du sitter alldeles där!
+                        </p>
+                      </div>
+
+                      <div className="column is-7"
+                        style={{
+                          backgroundImage: `url(${sax})`,
+                          backgroundSize: 'cover',
+                          backgroundPosition: 'center'
+                        }}
+                      >
+
+                      </div>
+
+                    </div>
+
                     <div className="content">
                       <div className="full-width-image-container margin-top-0" style={{
                         backgroundImage: 'url(https://via.placeholder.com/450)',
