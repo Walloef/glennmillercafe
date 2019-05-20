aec6aa636a9088b5e77cc46546ee63bb8de28414
ninjagrisen <kristofer@spoton.se>
2019-05-21 00:37:00 +0200

    reset stop style

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,36 +24,42 @@ class IndexPage extends React.Component {
 
         <Layout>
 
-          {currentLang}
+          {/* {currentLang}
 
 
           <a target="_blank" rel="noopener noreferrer" href="http://www.hitta.se/kartan/partner?mc=59.336751,18.070739">
             <img src="https://api.hitta.se/image/v2/0_2x/17?width=980&height=500&zoom.to=location&location=%7B%22id%22%3A%221010178402%22%7D?markers=%7B%22marker%22:1%7D" />
-          </a>
+          </a> */}
 
 
 
 
-          {/* <div style={{
-          textAlign: 'center'
-        }}>
-          <h1 style={{
-            fontSize: '2rem',
-            maxWidth: '500px',
-            margin: '0 auto'
-          }}
-          >Glenn Miller Cafés nya sida är under uppbyggnad</h1>
-          <p>Besök
+          <div style={{
+            textAlign: 'center',
+            position: 'absolute',
+            width: '100%',
+            top: '0',
+            zIndex: '999',
+            background: '#171834',
+            height: '70vh'
+          }}>
+            <h1 style={{
+              fontSize: '2rem',
+              maxWidth: '500px',
+              margin: '0 auto'
+            }}
+            >Glenn Miller Cafés nya sida är under uppbyggnad</h1>
+            <p>Besök
             <a href="http://www.glennmillerprogram.se/"
-              style={{
-                color: '#fdea4e',
-                textDecoration: 'underline',
-                marginLeft: '5px'
-              }}>
-              glennmillerprogram
+                style={{
+                  color: '#fdea4e',
+                  textDecoration: 'underline',
+                  marginLeft: '5px'
+                }}>
+                glennmillerprogram
             </a> för information om Glenn Miller Café
           </p>
-        </div> */}
+          </div>
         </Layout>
       </Fragment >
     )
