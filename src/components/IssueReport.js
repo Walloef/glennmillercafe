2355fa09c8150f4b7181071371b654a5be3b1345
ninjagrisen <kristofer@spoton.se>
2019-05-21 15:22:35 +0200

    issue null check

--- src/components/IssueReport.js
+++ src/components/IssueReport.js
@@ -12,17 +12,20 @@ const IssueReport = class extends React.Component {
         const svIssue = this.props.svIssue ?
             <p>{this.props.svIssue}</p>
             : ''
-        const Issue = localStorage.getItem('gmLang') === 'en' ? <Fragment>
-            <div className="issue__wrapper">
-                {enIssue}
-            </div>
-        </Fragment> :
-            <Fragment>
-                <div className="issue__wrapper">
-                    {svIssue}
-                </div>
-            </Fragment>;
+        let Issue = null;
+        if (typeof window !== `undefined`) {
 
+            Issue = window.localStorage.getItem('gmLang') === 'en' ? <Fragment>
+                <div className="issue__wrapper">
+                    {enIssue}
+                </div>
+            </Fragment> :
+                <Fragment>
+                    <div className="issue__wrapper">
+                        {svIssue}
+                    </div>
+                </Fragment>;
+        }
         return (
             <Fragment>{Issue}</Fragment>
         )
