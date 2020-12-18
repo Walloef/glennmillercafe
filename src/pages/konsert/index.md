4592f653db41e52b12bc21e60af3a748da94b3f4
ninjagrisen <kristofer@spoton.se>
2020-12-18 16:23:03 +0100

    added konsert page

---
+++ src/pages/konsert/index.md
@@ -0,0 +1,34 @@
+---
+templateKey: konsert-page
+title: Konserter
+---
+
+
+| Konserter i December 2020                                                                                                                                                                                       |       |
+| --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | ----- |
+| Lena Swanberg voc, Karin Hammar trb, Britta Swanberg violin, Ricard Krantz g, Robert Erlandsson b, Mats Persson dr. Livestreamas 19.30 på Facebook/Glenn Miller Café, vi öppnar kl 19 och kan ta 8 st matgäster | On 02 
+|Camilla Jonsson voc, Leo Lindberg p,  Björn Lundén b, Jonas Bäckman dr, Livestreamas 19.30 på Facebook/Glenn Miller Café, vi öppnar kl 19 och kan ta 8 st matgäster |To 03|
+|Restauranten är öppen från 18.00, ingen livejazz men jazz i högtalare|Fr 04|
+|Restauranten är öppen från 18.00, ingen livejazz men jazz i högtalare|Lö 05|Å
+|**Vladan Wirant trio**Vladan Wirant p, Niklas Fernqvist b, Daniel Fredriksson dr, Livestreamas 19.30 på Facebook/Glenn Miller Café, vi öppnar kl 19 och kan ta 8 st matgäster |Sön 06|
+|Linus Lindblom sax, Nils Janson trp, Martin Höper b,Konrad Agnas dr, Livestreamas 19.30 på Facebook/Glenn Miller Café, vi öppnar kl 19 och kan ta 8 st matgäster |On 09|
+|Karin Hammar trb, Linnea Jonsson trp, Calle Bagge p, Martin Höper b, Adam Ross dr, Livestreamas 19.30 på Facebook/Glenn Miller Café, vi öppnar kl 19 och kan ta 8 st matgäster |To 10|
+|Restauranten är öppen från 18.00, ingen livejazz men jazz i högtalare|Fr 11|
+|Luciajazz med Sofia Pettersson voc, Maria Kvist p, Robert Erlandsson b, trummor TBA, Livestreamas 19.30 på Facebook/Glenn Miller Café, vi öppnar kl 19 och kan ta 8 st matgäster |Lö 12|
+|**Svenska folkjazzkvartetten** Isak Hedtjärn cl, Jonas Liljeberg cl/sax, Vilhelm Bromander b, Anton Jonsson dr, Livestreamas 19.30 på Facebook/Glenn Miller Café, vi öppnar kl 19 och kan ta 8 st matgäster |Sö 13|
+|Klas Lindquist sax, Hans Backenroth b, Jonas Bäckman dr,  Livestreamas 19.30 på Facebook/Glenn Miller Café, vi öppnar kl 19 och kan ta 8 st matgäster |On 16|
+|Klas Lindquist sax, Niklas Fernqvist b, Daniel Fredriksson dr, Livestreamas 19.30 på Facebook/Glenn Miller Café, vi öppnar kl 19 och kan ta 8 st matgäster|TO 17|
+|Program kommer|Fre 18|
+|Kasper Agnas band (lineup Kommer)|Lö 19|
+|Nils Berg sax, Niclas Skagstedt p, Mauritz Agnas b, Konrad Agnas dr, Livestreamas 19.30 på Facebook/Glenn Miller Café, vi öppnar kl 19 och kan ta 8 st matgäster|Sön 20|
+|Katarina Henrysson voc, Svante Henrysson cello,Livestreamas 19.30 på Facebook/Glenn Miller Café, vi öppnar kl 19 och kan ta 8 st matgäster|Sön 27|
+   
+ 
+
+
+
+
+
+                                                                                                                                                   
+
+I samarbete med SENSUS
--- src/templates/konsert-page.js
+++ src/templates/konsert-page.js
@@ -1,58 +1,57 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import Content, { HTMLContent } from '../components/Content';
 
 export const KonsertPageTemplate = ({ title, content, contentComponent }) => {
-    const PageContent = contentComponent || Content
+  const PageContent = contentComponent || Content;
 
-    return (
-        <section className="section section--gradient">
-            <div className="container">
-                <div className="background-depending-on-page">
-                    <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                        {title}
-                    </h2>
-                </div>
-                <div className="columns">
-                    <div className="column is-10 is-offset-1">
-                        <div className="section">
-
-                            <PageContent className="content" content={content} />
-                        </div>
-                    </div>
-                </div>
+  return (
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="background-depending-on-page">
+          <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+            {title}
+          </h2>
+        </div>
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <PageContent className="content" content={content} />
             </div>
-        </section>
-    )
-}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 KonsertPageTemplate.propTypes = {
-    title: PropTypes.string.isRequired,
-    content: PropTypes.string,
-    contentComponent: PropTypes.func,
-}
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  contentComponent: PropTypes.func,
+};
 
 const KonsertPage = ({ data }) => {
-    const { markdownRemark: post } = data
+  const { markdownRemark: post } = data;
 
-    return (
-        <Layout>
-            <KonsertPageTemplate
-                contentComponent={HTMLContent}
-                title={post.frontmatter.title}
-                content={post.html}
-            />
-        </Layout>
-    )
-}
+  return (
+    <Layout>
+      <KonsertPageTemplate
+        contentComponent={HTMLContent}
+        title={post.frontmatter.title}
+        content={post.html}
+      />
+    </Layout>
+  );
+};
 
 KonsertPage.propTypes = {
-    data: PropTypes.object.isRequired,
-}
+  data: PropTypes.object.isRequired,
+};
 
-export default KonsertPage
+export default KonsertPage;
 
 export const aboutPageQuery = graphql`
   query KonsertPage($id: String!) {
@@ -63,4 +62,4 @@ export const aboutPageQuery = graphql`
       }
     }
   }
-`
+`;
